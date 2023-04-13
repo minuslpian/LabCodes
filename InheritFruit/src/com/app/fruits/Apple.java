b37f98855956eaaa4fa7bcc8f64334b535ab2be1
minuslpian <128726119+minuslpian@users.noreply.github.com>
2023-04-13 21:52:57 +0530

    Add files via upload

---
+++ InheritFruit/src/com/app/fruits/Apple.java
@@ -0,0 +1,22 @@
+package com.app.fruits;
+
+public class Apple extends Fruit {
+	
+	public Apple(String color,double weight,String name,boolean fresh) {
+		super(color,weight,name,fresh);
+	}
+	
+ @Override
+ public String taste() {
+	 return "Sweet and Sour";
+ }
+ 
+ public void jam() {
+	 System.out.println("Name : "+getName()+" Making jam");
+		
+	 
+ }
+ public void specFunc() {
+	 this.jam();
+ }
+}
---
+++ InheritFruit/src/com/app/tester/FruitBasket.java
@@ -0,0 +1,85 @@
+package com.app.tester;
+import com.app.fruits.*;
+import java.util.*;
+
+public class FruitBasket {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the quantity of basket :");
+		Scanner sc=new Scanner(System.in);
+		
+		Fruit arr[]=new Fruit[sc.nextInt()];
+		boolean choice=false;
+		int count=0;
+		while(!choice) {
+			System.out.println("Enter choice 1.Add Mango  2.Add Oranges  3.Add Apple  4.Display Fruit names "
+					+ "5.Display all from Fruits  6.modify Not Fresh  7.modify sour fruits Stale  8.Fruit Functionality  9.Exit");
+			
+		switch(sc.nextInt()) {
+		
+		case 1:
+			System.out.println("Color Weight Name");
+			
+			if(count< arr.length) 
+				arr[count++]=new Mango(sc.next(),sc.nextDouble(),sc.next(),true);
+
+			break;
+			
+		case 2:
+			System.out.println("Color Weight Name");
+			
+			if(count< arr.length) {
+					arr[count++]=new Orange(sc.next(),sc.nextDouble(),sc.next(),true);
+			}
+			break;
+		case 3:
+			System.out.println("Color Weight Name");
+			if(count< arr.length) {
+				arr[count++]=new Apple(sc.next(),sc.nextDouble(),sc.next(),true);
+			}
+			break;
+		case 4:
+			for(Fruit f :arr) {
+        		if(f!=null)
+        			System.out.println(f.getName());
+        	}
+			break;
+        case 5:
+        	for(Fruit f :arr) {
+        		if(f!=null)
+        			System.out.println(f);
+        	}
+        	break;
+        	
+		case 6:
+			System.out.println("Enter index of Fruit :");
+        	arr[sc.nextInt()-1].isFresh(false);
+			break;
+			
+		case 7:
+			for(Fruit f: arr)
+				if(f.taste().equals("sour"))
+					f.isFresh(false);
+			break;
+			
+        case 8:
+        	System.out.println("Enter index of Fruit :");
+        	Fruit f= arr[sc.nextInt()-1];
+        	if(f instanceof Mango)
+        		
+        		((Mango)f).pulp();
+        	else if(f instanceof Apple)
+        		((Apple)f).jam();
+        	else
+        		((Orange)f).juice();
+        	
+        	break;
+		case 9:
+			choice=true;
+			break;
+		}
+		
+		}
+	}
+
+}
