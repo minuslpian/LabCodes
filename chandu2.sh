e9416a5273bdbc09a57a22360d19422f5745303b
minuslpian <128726119+minuslpian@users.noreply.github.com>
2023-04-25 23:59:23 +0530

    Update chandu2.sh

--- chandu2.sh
+++ chandu2.sh
@@ -1,64 +1,82 @@
 #!/bin/bash
-read -p "Enter filename " fname;
+
 PS3="Enter from menu 1.create file 2.display contents 3.copy files 4.rename 5.delete 0.exit"
 select i in 1 2 3 4 5 6
 do
 	case $i in
 	1) 
-		if [[ -f $fname ]]
+		read -p "Enter filename " fname
+		if [[ -e $fname ]]
 		then
-		echo" File already exists. Enter new name";
+		echo" File already exists. Enter new name"
 	else
-		var=`touch $fname`;
-		echo " file created by name $fname";
+		echo "Enter contents to add to file and press ctrl+d"
+		cat > "$fname"
+		echo " file created by name $fname"
 		fi
 	;;
 	2)
-		if [[ -f $fname ]]
+		read -p "Enter filename " fname
+		if [[ -e $fname ]]
 		then
-			var=`cat $fname`
-			echo "$var";
+			cat $fname
 		else
 			echo "file doesn't exist";
 		fi		
 		;;
 	3)
-		read -p "Enter filename to copy" fname2;
-		if [[ -f $fname2 ]]
+		read -p "Enter src filename " fname
+		
+		if [[ !-e $fname2 ]]
 		then
-			`cp $fname $fname2`
+			echo "file doesn't exist";
+			
+		else if [[ !-r $fname2 ]]
+		then
+			echo "file is not readable"
 		else
-			echo "file doesn't exist. Creating new file..";
-			`touch $fname2`
-			`cp $fname $fname2`;
+		read -p "Enter filename to copy" fname2
 		fi
-		echo "done ";
-		break;;
+		if [[ -e $fname2 ]]
+		then
+			echo "dest file already exists"
+		else
+			cp $fname $fname2
+			echo "copying done"
+		fi
+		;;
 	4)
 		read -p "Enter filename and rename" fname3 fname4;
-		if [[ -f $fname3 ]]
+		if [[ -e $fname3 ]]
 		then
-			`mv $fname3 $fname4`
+			if [[ -e fname4 ]]
+			then
+				echo "renamed file already exists"
+			else
+				mv $fname3 $fname4
+			fi
 		else
 			echo "file doesn't exist!!!";
 		fi
-		break;;
+		;;
 	5)
 		read -p "Enter filename to delete" fname5;
-		if [[ -f $fname5 ]]
+		if [[ -e $fname5 ]]
 		then
-			`rm $fname5`;
+			if [[ -w $fname5 ]]
+			then
+				rm $fname5
+				echo "done deletion"
+			else
+				echo"file is not writable"
+			fi
 		else
-			echo "file doesn't exist!!!";
+			echo "file doesn't exist!!!"
 		fi
-		break;;
+		;;
 
 	*)
 		exit;;
-
-
-
-
 
 
 esac
